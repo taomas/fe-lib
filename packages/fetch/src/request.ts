a60f69bdb86eabb4d44c473f6c94a2204e8c23f0
taomasliu <taomasliu@tencent.com>
2021-04-29 20:14:37 +0800

    feat: add react example

--- packages/fetch/src/request.ts
+++ packages/fetch/src/request.ts
@@ -1,28 +1,17 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 
 export default class Ajax {
-  private baseUrl = "/"
+  private baseUrl = '/'
 
   private request(params: AxiosRequestConfig): Promise<any> {
     return new Promise((resolve, reject) => {
       axios(params)
         .then((res: AxiosResponse) => {
-          if (res && res.status === 400) {
-            console.log("前端传参异常了~")
-          } else if (res.data) {
-            if (res.data?.code === 0 || res.data?.ret === 0) {
-              resolve(res.data.data)
-            } else {
-              reject(res.data.msg)
-            }
-          }
+          resolve(res.data)
         })
         .catch((err) => {
           const error = err?.response
-          if (error.status === 400) {
-            console.log("前端传参异常了~")
-          }
-          reject(err)
+          reject(error)
         })
     })
   }
@@ -30,24 +19,24 @@ export default class Ajax {
   private queryString(url: string, query?: Record<string, string>): string {
     const str = []
     for (const key in query) {
-      str.push(key + "=" + query[key])
+      str.push(key + '=' + query[key])
     }
-    const paramStr = str.join("&")
+    const paramStr = str.join('&')
     return paramStr ? `${url}?${paramStr}` : url
   }
 
-  public get(url = "", params?: Record<string, string>): Promise<any> {
+  public get(url = '', params?: Record<string, string>): Promise<any> {
     return this.request({
-      method: "get",
-      url: this.queryString(`${this.baseUrl}${url}`, params),
+      method: 'get',
+      url: this.queryString(`${this.baseUrl}${url}`, params)
     })
   }
 
   public post(url: string, params?: Record<string, any>): Promise<any> {
     return this.request({
-      method: "post",
+      method: 'post',
       url: `${this.baseUrl}${url}`,
-      data: params,
+      data: params
     })
   }
 }
