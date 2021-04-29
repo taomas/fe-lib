ea8e44304b6c8e914b6b831046c3a69fca5dac75
taomasliu <taomasliu@tencent.com>
2021-04-29 21:02:54 +0800

    fix: 更新storybook文档

--- example/react/src/stories/Header.stories.tsx
+++ example/react/src/stories/Header.stories.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import { Story, Meta } from '@storybook/react';
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
 
-import { Header, HeaderProps } from './Header';
+import { Header, HeaderProps } from './Header'
 
 export default {
   title: 'Example/Header',
-  component: Header,
-} as Meta;
+  component: Header
+} as Meta
 
-const Template: Story<HeaderProps> = (args) => <Header {...args} />;
+const Template: Story<HeaderProps> = (args) => <Header {...args} />
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  user: {},
-};
+const onLogin = () => {
+  console.log('onLogin')
+}
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+const onLogout = () => {
+  console.log('onLogout')
+}
+
+const onCreateAccount = () => {
+  console.log('onCreateAccount')
+}
+
+const loginUser = {
+  name: '张三'
+}
+
+export const LoggedIn = () => (
+  <Header
+    onLogin={onLogin}
+    onLogout={onLogout}
+    onCreateAccount={onCreateAccount}
+  />
+)
+
+export const LoggedOut = () => (
+  <Header
+    user={loginUser}
+    onLogin={onLogin}
+    onLogout={onLogout}
+    onCreateAccount={onCreateAccount}
+  />
+)
